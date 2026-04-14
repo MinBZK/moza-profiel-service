724671989713a73950f76f8746dece7ffcd732e7
TijmenVDBrink <tijmen.brink@rijksoverheid.nl>
2026-04-14 15:23:56 +0200

    Staan nu in actualiteiten service

--- src/main/java/nl/rijksoverheid/moz/common/VoorkeurType.java
+++ src/main/java/nl/rijksoverheid/moz/common/VoorkeurType.java
@@ -3,7 +3,5 @@ package nl.rijksoverheid.moz.common;
 public enum VoorkeurType {
     WebsiteTaal,
     MagGebeldWorden,
-    WebsiteThema,
-    PostcodeInUwBuurt,
-    ActueleOnderwerpVoorkeur
+    WebsiteThema
 }
