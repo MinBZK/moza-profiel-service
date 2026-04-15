0c34ba1b8bce357812e3677a9bca94660c188d10
Mick Hoogendam <mick.hoogendam@rijksoverheid.nl>
2026-04-15 11:04:04 +0200

    Refactored previous commit about toggle for email verificatie service

--- src/main/java/nl/rijksoverheid/moz/clients/mock/verificatie_service/MockVerificationControllerApi.java
+++ src/main/java/nl/rijksoverheid/moz/clients/mock/verificatie_service/MockVerificationControllerApi.java
@@ -1,39 +1,35 @@
-package nl.rijksoverheid.moz.external.clients.verificatie_service.mock;
+package nl.rijksoverheid.moz.clients.mock.verificatie_service;
 
+import io.quarkus.arc.profile.UnlessBuildProfile;
 import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Alternative;
+
+import java.util.UUID;
+
 import nl.rijksoverheid.moz.external.clients.verificatie_service.api.VerificationControllerApi;
 import nl.rijksoverheid.moz.external.clients.verificatie_service.model.VerificationApplicationRequest;
 import nl.rijksoverheid.moz.external.clients.verificatie_service.model.VerificationRequest;
 import nl.rijksoverheid.moz.external.clients.verificatie_service.model.VerificationResponse;
-import nl.rijksoverheid.moz.services.EmailVerificatieService;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
-import io.quarkus.arc.profile.IfBuildProfile;
-import org.jboss.logging.Logger;
 
-import java.util.UUID;
-
-@Alternative
-@Priority(1)
 @ApplicationScoped
 @RestClient
-@IfBuildProfile("test")
-public class VerificationControllerMock implements VerificationControllerApi {
-
-    private static final org.jboss.logging.Logger LOG = Logger.getLogger(VerificationControllerMock.class);
+@Alternative
+@Priority(1)
+@UnlessBuildProfile("prod")
+public class MockVerificationControllerApi implements VerificationControllerApi {
 
     @Override
     public String requestPost(VerificationApplicationRequest verificationApplicationRequest) {
-        LOG.debug("Mocked request to verification service: " + verificationApplicationRequest);
         return UUID.randomUUID().toString();
     }
 
     @Override
     public VerificationResponse verifyPost(VerificationRequest verificationRequest) {
-        LOG.debug("Mocked verification request: " + verificationRequest);
         VerificationResponse response = new VerificationResponse();
         response.setSuccess(true);
         return response;
     }
+
 }
