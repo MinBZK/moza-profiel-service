ca4d4f5fd61dbe7524d6589ba7b097945902f518
Mick Hoogendam <mick.hoogendam@rijksoverheid.nl>
2026-04-15 11:04:36 +0200

    cleaned up editor warnings

--- src/main/java/nl/rijksoverheid/moz/services/Startup.java
+++ src/main/java/nl/rijksoverheid/moz/services/Startup.java
@@ -1,7 +1,5 @@
 package nl.rijksoverheid.moz.services;
 
-
-import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
