c9d2d17dfc799ae686a8385bbe8d6b0e46bbafa9
Joeri Taelman-Kroon <joeri.taelman@rijksoverheid.nl>
2026-06-09 13:37:19 +0200

    Responding to CodeQuality comment

--- src/main/java/nl/rijksoverheid/moz/controller/DomainExceptionMapper.java
+++ src/main/java/nl/rijksoverheid/moz/controller/DomainExceptionMapper.java
@@ -13,7 +13,7 @@ public class DomainExceptionMapper {
     private static final Logger LOG = Logger.getLogger(DomainExceptionMapper.class);
 
     @ServerExceptionMapper
-    public Response map(BusinessException e) {
+    public Response mapBusinessException(BusinessException e) {
         Response.Status status = switch (e.getKind()) {
             case NOT_FOUND -> Response.Status.NOT_FOUND;
             case CONFLICT -> Response.Status.CONFLICT;
@@ -23,18 +23,18 @@ public class DomainExceptionMapper {
     }
 
     @ServerExceptionMapper
-    public Response map(TechnicalException e) {
+    public Response mapTechnicalException(TechnicalException e) {
         LOG.error("Technische fout: " + e.getDetail(), e);
         return Problems.problemResponse(Response.Status.INTERNAL_SERVER_ERROR, e.getTitle(), e.getDetail());
     }
 
     @ServerExceptionMapper
-    public Response map(AuthorizationException e) {
+    public Response mapAuthorizationException(AuthorizationException e) {
         return Problems.problemResponse(Response.Status.FORBIDDEN, e.getTitle(), e.getDetail());
     }
 
     @ServerExceptionMapper
-    public Response map(Exception e) {
+    public Response mapUnhandledException(Exception e) {
         LOG.error("Onverwachte fout opgetreden", e);
         return Problems.problemResponse(
                 Response.Status.INTERNAL_SERVER_ERROR,
